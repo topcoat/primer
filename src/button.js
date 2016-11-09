d78a11262140898e3885c36bee8899164b352f00
garthdb <garthdb@gmail.com>
2016-11-09 13:33:28 -0700

    :sparkles: Added states to button

--- src/button.js
+++ src/button.js
@@ -1,7 +1,9 @@
 import Component from './types/component';
 import ButtonBase from './button-base';
+import {DisabledMixin} from './utils';
 
 export default class ButtonComponent extends Component {
+  description = 'A simple button';
   mixins = [ButtonBase];
   props = {
     @variable
@@ -22,5 +24,33 @@ export default class ButtonComponent extends Component {
     verticalAlign: '',
     @variable
     borderRadius: '5px',
+  };
+  states = {
+    hover: {
+      props: {
+        @variable('background-color--hover')
+        backgroundColor: '',
+      },
+    },
+    active: {
+      props: {
+        @variable('background-color--down')
+        backgroundColor: '',
+        @variable('box-shadow--down')
+        boxShadow: '',
+      },
+    },
+    focus: {
+      props: {
+        @variable('border--focus')
+        border: '',
+        @variable('box-shadow--focus')
+        boxShadow: '',
+        outline: 0,
+      },
+    },
+    disabled: {
+      mixins: [DisabledMixin],
+    },
   }
 }
